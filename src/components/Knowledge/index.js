a521e1d77ccd1f5355aee5d980e391d2a1e9fa30
Pedro Magalhães <pedro@scrumlaunch.com>
2023-05-02 23:28:32 -0300

    added medium and youtube

--- src/components/Knowledge/index.js
+++ src/components/Knowledge/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import { StyledSectionKnowledge } from '@/styles/sections';
 import { StyledKnowledgeBase, StyledIcons } from './styles';
 import { FaYoutube, FaMedium } from 'react-icons/fa';
+import Link from 'next/link';
 
 export default function KnowledgeBase() {
   return (
@@ -19,8 +20,12 @@ export default function KnowledgeBase() {
             <h2>Stay Ahead with Our Comprehensive Blockchain Knowledge Base</h2>
             <p>We believe in the power of knowledge sharing. Explore our extensive library of resources, including articles, case studies, whitepapers, and webinars, to deepen your understanding of blockchain technology and its potential impact on your industry.</p>
             <StyledIcons>
-              <FaYoutube size={40} />
-              <FaMedium size={40} />
+              <Link href="https://www.youtube.com/@pemagalhaes_" passHref>
+                <FaYoutube size={40} />
+              </Link>
+              <Link href="https://medium.com/@ioralabs" passHref>
+                <FaMedium size={40} />
+              </Link>
             </StyledIcons>
           </div>
         </div>
